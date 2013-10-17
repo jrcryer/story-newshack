885f70061b5e55a0c7a477bc9c34bda9204929d0
James Cryer <info@jamescryer.com>
2013-10-17 13:48:21 +0100

    Adding front-end MVC

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+/*global require */
+require.config({
+    paths: {
+        jquery: '/lib/jquery/jquery',
+        app: 'hack/app',
+        underscore: '/lib/underscore/underscore',
+        backbone: '/lib/backbone/backbone'
+    },
+    shim: {
+        'underscore': {
+            exports: '_'
+        },
+        'jquery': {
+            exports: 'jQuery'
+        },
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'hack/app': {
+            exports: 'Hack'
+        }
+    }
+});
+
+require([
+    'jquery',
+    'backbone',
+    'hack/app',
+], function($, Backbone, Hack) {
+
+
+});
