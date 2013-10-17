07a30792a3cd92ca3a0c67592d810eabc48c56e6
Kevin Bradwick <kevinbradwick@gmail.com>
2013-10-17 15:46:21 +0100

    added /map route to debug ol3

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,12 +2,22 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    async = require('async'),
-    _ = require('underscore');
+  async = require('async'),
+  _ = require('underscore');
 
 
 exports.render = function(req, res) {
-    res.render('index', {
-        user: req.user ? JSON.stringify(req.user) : "null"
-    });
+  res.render('index', {
+    user: req.user ? JSON.stringify(req.user) : "null"
+  });
+};
+
+/**
+ * This is just a test function
+ *
+ * @param {Object} req
+ * @param {Object} res
+ */
+exports.map = function(req, res) {
+  res.render('map');
 };
--- config/routes.js
+++ config/routes.js
@@ -5,4 +5,5 @@ module.exports = function(app) {
     //Home route
     var index = require('../app/controllers/index');
     app.get('/', index.render);
+    app.get('/map', index.map);
 };
