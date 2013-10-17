9f8fb9a257a76446e269cef5f71eae54f34f0eac
Darren Lucas <darren.lucas@bbc.co.uk>
2013-10-17 13:30:18 +0100

    Fixing config

--- config/env/all.js
+++ config/env/all.js
@@ -3,7 +3,7 @@ rootPath = path.normalize(__dirname + '/../..');
 
 module.exports = {
     root: rootPath,
-    ip: process.env.OPENSHIFT_NODEJS_IP || 127.0.0.1,
+    ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     port: process.env.OPENSHIFT_NODEJS_PORT || 3000,
     db: process.env.OPENSHIFT_MONGODB_DB_URL
 }
