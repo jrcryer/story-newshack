b36d6e218797eed0a8a8b648ade8da41374241ad
Kevin Bradwick <kevinbradwick@gmail.com>
2013-10-17 19:54:24 +0100

    added media manager to window

--- public/js/app.js
+++ public/js/app.js
@@ -1,67 +1,67 @@
 /*global require */
 require.config({
-    paths: {
-        jquery: '/lib/jquery/jquery',
-        app: 'hack/app',
-        underscore: '/lib/underscore/underscore',
-        backbone: '/lib/backbone/backbone'
+  paths: {
+    jquery: '/lib/jquery/jquery',
+    app: 'hack/app',
+    underscore: '/lib/underscore/underscore',
+    backbone: '/lib/backbone/backbone'
+  },
+  shim: {
+    'underscore': {
+      exports: '_'
     },
-    shim: {
-        'underscore': {
-            exports: '_'
-        },
-        'jquery': {
-            exports: 'jQuery'
-        },
-        'backbone': {
-            deps: ['underscore', 'jquery'],
-            exports: 'Backbone'
-        },
-        'hack/app': {
-            exports: 'Hack'
-        }
+    'jquery': {
+      exports: 'jQuery'
+    },
+    'backbone': {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    'hack/app': {
+      exports: 'Hack'
     }
+  }
 });
 
 require([
-    'jquery',
-    'backbone',
-    'hack/app',
+  'jquery',
+  'backbone',
+  'hack/app',
 ], function($, Backbone, Hack) {
 
-    $(function() {
+  $(function() {
 
-        var mediaManager;
+    var mediaManager;
 
-        $.ajax({
-            url: 'data/stories/arab_spring.json',
-            success: function(config) {
-                init(config);
-            },
-            error: function() {
-                console.log('Error loading config.');
-            }
-        });
-
-        function init(config) {
-
-            mediaManager = new Hack.MediaManager(config);
-            mediaManager.setPage(config.storyline.chapters[0], config.storyline.chapters[0].pages[0]);
-
-            var chapterPanel = new Hack.ChapterPanel({
-                el: '#chapter-panel',
-                storyline: config.storyline
-            });
-            var contentPanel = new Hack.ContentPanel({
-                el: '#content-panel'
-            });
-            var panelMgr = new Hack.PanelManager();
-            panelMgr.add(chapterPanel);
-            panelMgr.add(contentPanel);
-            panelMgr.start();
-
-        }
-
+    $.ajax({
+      url: 'data/stories/arab_spring.json',
+      success: function(config) {
+        init(config);
+      },
+      error: function() {
+        console.log('Error loading config.');
+      }
     });
+
+    function init(config) {
+
+      mediaManager = new Hack.MediaManager(config);
+      mediaManager.setPage(config.storyline.chapters[0], config.storyline.chapters[0].pages[0]);
+
+      var chapterPanel = new Hack.ChapterPanel({
+        el: '#chapter-panel',
+        storyline: config.storyline
+      });
+      var contentPanel = new Hack.ContentPanel({
+        el: '#content-panel'
+      });
+      var panelMgr = new Hack.PanelManager();
+      panelMgr.add(chapterPanel);
+      panelMgr.add(contentPanel);
+      panelMgr.start();
+
+      window.mm = mediaManager;
+    }
+  });
 
 });
